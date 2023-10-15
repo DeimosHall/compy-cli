62ca36e10a7d3e093b79ad33a16ab84b9a37eec6
deimos <deimoshall@gmail.com>
2023-10-15 12:48:13 -0600

    feat: ask to delete original file

--- main.py
+++ main.py
@@ -28,11 +28,25 @@ if len(sys.argv) < 3:
 else:
     output_file = sys.argv[2]
 
-print(f'Input file: {input_file}')
-print(f'Output file: {output_file}')
+# Ask the user if he wants to delete the original file
+answer = input('Do you want to delete the original file? (y/n): ')
 
 # Build the ffmpeg command with the provided arguments
-command = f'ffmpeg -i "{input_file}" -vcodec libx264 -crf 23 -acodec aac -b:a 128k -map_metadata 0 "{output_file}"'
+hide_banner = '-v warning -hide_banner -stats'
+command = f'ffmpeg -i "{input_file}" {hide_banner} -vcodec libx264 -crf 23 -acodec aac -b:a 128k -map_metadata 0 "{output_file}"'
 
 # Run the command
 subprocess.call(command, shell=True)
+
+if answer == 'y':
+    # Delete the original file and remove the compressed word from the output file
+    original_file = input_file
+    input_size = subprocess.check_output(f'du -sh "{input_file}"', shell=True).split()[0].decode('utf-8')
+    output_size = subprocess.check_output(f'du -sh "{output_file}"', shell=True).split()[0].decode('utf-8')
+    subprocess.call(f'rm "{input_file}"', shell=True)
+    subprocess.call(f'mv "{output_file}" "{original_file}"', shell=True)
+    print(f'Original file size: {input_size}')
+    print(f'Compressed file size: {output_size}')
+    print('Original file deleted')
+else:
+    print('Original file not deleted')
