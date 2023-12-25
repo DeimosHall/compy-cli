09f19a4520b17c5558352e0620b4ba49d0e22de0
deimos <deimoshall@gmail.com>
2023-12-24 18:05:14 -0600

    fix: ignore the first dot for the output file name

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ except:
 
 input_file = ''
 output_file = ''
+output_file_provided = False
 
 # Check if the user provided the input file
 if len(sys.argv) < 2:
@@ -24,12 +25,12 @@ input_file = sys.argv[1]
 
 # Check if the user provided the output file
 if len(sys.argv) < 3:
-    output_file = f'{input_file.split(".")[0]} compressed.mp4'
+    output_file = f'{input_file[::-1].split(".", 1)[1][::-1]} compressed.mp4' # Get the file name without the extension and add compressed.mp4
 else:
+    output_file_provided = True
     output_file = sys.argv[2]
 
-# Ask the user if he wants to delete the original file
-answer = input('Do you want to delete the original file? (y/n): ')
+delete_original_file = input('Do you want to delete the original file? (y/n): ')
 
 # Build the ffmpeg command with the provided arguments
 hide_banner = '-v warning -hide_banner -stats'
@@ -38,13 +39,14 @@ command = f'ffmpeg -i "{input_file}" {hide_banner} -vcodec libx264 -crf 23 -acod
 # Run the command
 subprocess.call(command, shell=True)
 
-if answer == 'y':
+if delete_original_file == 'y':
     # Delete the original file and remove the compressed word from the output file
     original_file = input_file
     input_size = subprocess.check_output(f'du -sh "{input_file}"', shell=True).split()[0].decode('utf-8')
     output_size = subprocess.check_output(f'du -sh "{output_file}"', shell=True).split()[0].decode('utf-8')
     subprocess.call(f'rm "{input_file}"', shell=True)
-    subprocess.call(f'mv "{output_file}" "{original_file}"', shell=True)
+    if not output_file_provided:
+        subprocess.call(f'mv "{output_file}" "{original_file}"', shell=True)
     print(f'Original file size: {input_size}')
     print(f'Compressed file size: {output_size}')
     print('Original file deleted')
