9e5f422fb3dd5a3ca142f54153d8e9b234a91345
deimos <deimoshall@gmail.com>
2025-12-17 16:50:56 -0600

    feat: list all files

--- compy-cli/src/main.rs
+++ compy-cli/src/main.rs
@@ -1,6 +1,7 @@
 use std::{error::Error, process};
-use std::path::PathBuf;
+use std::path::{PathBuf};
 use clap::{Parser};
+use walkdir::WalkDir;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -36,6 +37,22 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     };
     println!("Input: {:?}", input);
+    
+    let mut files: Vec<PathBuf> = vec![];
+    
+    if input.exists() && input.is_file() {
+        files.push(input);
+    } else if input.exists() && input.is_dir() {
+        let directory: WalkDir = WalkDir::new(input.as_os_str());
+        for file in directory {
+            files.push(file?.into_path());
+        }
+    }
+    
+    println!("Files list:");
+    for file in files {
+        println!("File: {:?}", file);
+    }
     
     cli.delete.then(|| { delete_original() });
     cli.list.then(|| { list_files() });
