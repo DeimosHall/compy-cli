293bd96440be91e9d3c06085082100254a9627f8
deimos <deimoshall@gmail.com>
2025-12-16 21:09:59 -0600

    feat: panics if no input is provided

--- compy-cli/src/main.rs
+++ compy-cli/src/main.rs
@@ -1,9 +1,40 @@
 use std::error::Error;
+use std::path::PathBuf;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Optional name to operate on
+    input: Option<PathBuf>,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Work in progress...");
+    let cli = Cli::parse();
     
-    
+    let input = cli.input.expect("No input provided");
+    println!("Input: {:?}", input);
     
     Ok(())
 }
